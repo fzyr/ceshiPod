388c9252537394ffc90c029da3f7f28711bb667a
Johnson <shunmian@gmail.com>
2015-11-30 08:59:46 +0800

    Initial Commit

---
+++ ceshiPod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshiPod
+//
+//  Created by LAL on 15/11/30.
+//  Copyright © 2015年 LAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ceshiPod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshiPod
+//
+//  Created by LAL on 15/11/30.
+//  Copyright © 2015年 LAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
